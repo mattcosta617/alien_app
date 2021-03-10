48b977287013f5d540dbeff5e7682a2f2e8d84ea
mattcosta617 <mattiecosta@gmail.com>
2021-03-10 06:31:20 -0700

    Marquis's playground added along with styling to navigation

---
+++ app.js
@@ -0,0 +1,9 @@
+for ( let i = 0; i < 20; i ++) {
+    if(i < 5) {
+        console.log("Hello!");
+    } else if(i < 10) {
+        console.log("Middle man");
+    } else if(i > 10){
+        console.log("All done");
+    }
+}
